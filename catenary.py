26d62e00a1c61597edf0f46d91322085b442a4ec
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2019-08-06 22:13:58 +0200

    correction of code

--- catenary.py
+++ catenary.py
@@ -1,37 +1,36 @@
 #code to get the vertical balance of multiple catenaries
+#formulas obtained from http://www.mecapedia.uji.es/calculo_de_la_catenaria_conocidos_los_puntos_de_amarre_y_la_longitud.htm
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 
 #initialization of distances
-HorDif=120; xB2=120 #horizontal distance between extremes
-S=200 #length of the catenary
+HorDif=120 #horizontal distance between extremes [cm]
+VerDif=20 #vertical distance between extremes [cm]
+S=200 #length of the catenary in cm
 w=0.005 #weight per unit length
-vectorX1=np.linspace(0,HorDif,100) #create a vector of X values
-vectorX2=np.linspace(0,xB2,100)
-#yB1=descenso(Sab,xB1,w);
+vectorX1=np.linspace(0,HorDif,100) #X points of the catenary
 
 
-
+#catenary calculus function
 def catenary(HorDif,VerDif,S,VectorX):
+    
+    #calculus of x0 and y0 (see the reference web)
+    
     c=3 #initialization
 
-    f1=math.sqrt(S^2 - VerDif^2)/HorDif
+    f1=math.sqrt(S**2 - VerDif**2)/HorDif
     f2=math.sinh(HorDif/(2*c))/HorDif*(2*c)
 
 
     while (abs(f2-f1)>0.01):
         c=c+0.01
-        f1=math.sqrt(S^2 - VerDif^2)/HorDif
+        f1=math.sqrt(S**2 - VerDif**2)/HorDif
         f2=math.sinh(HorDif/(2*c))/HorDif*(2*c)
 
-    print("ya esta 3")
-    #ahora consigo la x0, y0
-    y0=c
-
-    #consigo x0
-    x0=3#initialization
+        
+    x0=3 #initialization
 
     f3= c*math.cosh((HorDif-x0)/c) - c*math.cosh(x0/c)
 
@@ -39,18 +38,18 @@ def catenary(HorDif,VerDif,S,VectorX):
         x0=x0+0.01
         f3= c*math.cosh((HorDif-x0)/c) - c*math.cosh(x0/c)
 
-    print("ya esta 2")
-    #hallo y0
+    
     y0 = VerDif - c*math.cosh((HorDif-x0)/c)
 
-    #dibujo la catenaria
+    #generate the y points of the catenary
     y=y0 + c*math.cosh((VectorX-x0)/c)
 
     return y,x0,y0,c
 
 
-#catenarias sin equilibrar
-y,x0,y0,c= catenary(HorDif, 0, S, vectorX1)
+#calculate the parameters of the catenary
+y,x0,y0,c= catenary(HorDif, VerDif, S, vectorX1)
 
-
-print("ya esta")
+#draw the catenary
+plt.plot(vectorX1,y)
+plt.show()
