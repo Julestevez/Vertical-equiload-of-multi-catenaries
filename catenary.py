9d1a5829dfdea899ed65b2f2f3506c91f0263a74
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2019-08-08 22:02:37 +0200

    trying to draw to catenaries
    intermediate update of the code to include another catenary and automatic calculus of vertical descend

--- catenary.py
+++ catenary.py
@@ -1,9 +1,11 @@
 #code to get the vertical balance of multiple catenaries
 #formulas obtained from http://www.mecapedia.uji.es/calculo_de_la_catenaria_conocidos_los_puntos_de_amarre_y_la_longitud.htm
+#hyperbolic functions http://mpmath.org/doc/current/functions/hyperbolic.html
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import simpy.mpmath
+
+from mpmath import *
 
 
 #initialization of distances
@@ -11,7 +13,8 @@ HorDif=120 #horizontal distance between extremes [cm]
 VerDif=20 #vertical distance between extremes [cm]
 S=200 #length of the catenary in cm
 w=0.005 #weight per unit length [kg/cm]
-vectorX1=np.linspace(0,HorDif,100) #X points of the catenary
+vectorX1=np.linspace(0,HorDif,100) #X points of the first catenary
+vectorX2=np.linspace(HorDif,2*HorDif,100) #X points of the second catenary
 
 
 #calculus of vertical descent for equilibrum
@@ -22,30 +25,32 @@ def VerticalDescend(L0,Lx,w):
 
     iter2=1
 
-    F1_1=w/2*(-Y2*math.coth(expresion_A)+L0)-2*w*L0/3
-    F2_1=w/2*(-math.coth(expresion_A)-(math.sqrt(3)*Y2**2*(mpmath.csch(expresion_A))**2/((Lx**2)*expresion_A/math.sqrt(3))))
+    F1_1=w/2*(-Y2*coth(expresion_A)+L0)-2*w*L0/3
+    F2_1=w/2*(-coth(expresion_A)-(math.sqrt(3)*Y2**2*(csch(expresion_A))**2/((Lx**2)*expresion_A/math.sqrt(3))))
 
     Y2=Y2-F1_1/F2_1
-    F3_1=w/2*(-Y2*math.coth(expresion_A)+L0)-2*w*L0/3
+    F3_1=w/2*(-Y2*coth(expresion_A)+L0)-2*w*L0/3
 
 
     while(abs(F1_1-F3_1)>0.0000000001 and abs(F1_1)>0.0001):
    
         #función que dice que un extremo tiene que cargar la 1/3 del peso total de 2 catenarias
-        F1_1=w/2*(-Y2*math.coth(expresion_A)+L0)-2*w*L0/3
+        F1_1=w/2*(-Y2*coth(expresion_A)+L0)-2*w*L0/3
    
         #derivative of F1_1
-        F2_1=w/2*(-math.coth(expresion_A)-(sqrt(3)*Y2**2*(mpmath.csch(expresion_A))**2/((Lx**2)*expresion_A/sqrt(3))))
+        F2_1=w/2*(-coth(expresion_A)-(math.sqrt(3)*Y2**2*(csch(expresion_A))**2/((Lx**2)*expresion_A/math.sqrt(3))))
     
         #manual implementation of Newton-Raphson
         Y2=Y2-F1_1/F2_1
-        F3_1=w/2*(-Y2*math.coth(expresion_A)+L0)-2*w*L0/3
+        F3_1=w/2*(-Y2*coth(expresion_A)+L0)-2*w*L0/3
         iter2=iter2+1
+
+    return Y2
 
 
 #catenary calculus function
 def catenary(HorDif,VerDif,S,VectorX):
-    print(dibujando)
+
     #calculus of x0 and y0 (see the reference web)
 
     c=3 #initialization
@@ -78,11 +83,14 @@ def catenary(HorDif,VerDif,S,VectorX):
     return y,x0,y0,c
 
 
+
+
 #calculate the parameters of the catenary
 VerDif=VerticalDescend(S,HorDif,w)
 y,x0,y0,c= catenary(HorDif, VerDif, S, vectorX1)
+#y1,x01,y01,c1 = catenary(HorDif,-VerDif, S, vectorX2)
 
 #draw the catenary
 plt.plot(vectorX1,y)
+#plt.plot(vectorX2,y1)
 plt.show()
-
