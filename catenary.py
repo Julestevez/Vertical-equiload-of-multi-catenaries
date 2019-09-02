ffa2bbcc2496213c631b5301f679ecd5e131ee2c
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2019-09-02 13:35:41 +0200

    added animation to get the balance situation

--- catenary.py
+++ catenary.py
@@ -17,6 +17,25 @@ vectorX1=np.linspace(0,HorDif,100) #X points of the first catenary
 
 
 
+#code to get the vertical balance of multiple catenaries
+#formulas obtained from http://www.mecapedia.uji.es/calculo_de_la_catenaria_conocidos_los_puntos_de_amarre_y_la_longitud.htm
+#hyperbolic functions http://mpmath.org/doc/current/functions/hyperbolic.html
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+from mpmath import *
+
+
+#initialization of distances
+HorDif=120 #horizontal distance between extremes [cm]
+VerDif=20 #vertical distance between extremes [cm]
+S=200 #length of the catenary in cm
+w=0.005 #weight per unit length [kg/cm]
+vectorX1=np.linspace(0,HorDif,100) #X points of the first catenary
+
+
+
 #calculus of vertical descent for equilibrum
 def VerticalDescend(L0,Lx,w):
 
@@ -34,7 +53,7 @@ def VerticalDescend(L0,Lx,w):
 
     while(abs(F1_1-F3_1)>0.0000000001 and abs(F1_1)>0.0001):
    
-        #función que dice que un extremo tiene que cargar la 1/3 del peso total de 2 catenarias
+        #this function states that each point of the catenary must load 1/3 of the total weight
         F1_1=w/2*(-Y2*coth(expresion_A)+L0)-2*w*L0/3
    
         #derivative of F1_1
@@ -89,15 +108,22 @@ y0,x0,y_0,c0= catenary(HorDif, 0, S, vectorX1)
 y1,x01,y_1,c1 = catenary(HorDif,0, S, vectorX1)
 
 
-#calculate the parameters of the final catenary
+#calculate the parameters of the final catenary with an animation
 VerDif=VerticalDescend(S,HorDif,w)
-y2,x2,y_2,c2= catenary(HorDif, VerDif, S, vectorX1)
-y3,x03,y_3,c3 = catenary(HorDif,-VerDif, S, vectorX1)
+VerDif_vector=np.linspace(0,float(VerDif),5)
 
-#draw the catenary
-plt.plot(vectorX1,y0,'b')
-plt.plot(vectorX1+HorDif,y1,'b')
+for i in range(0,4):
+    y2,x2,y_2,c2= catenary(HorDif, VerDif_vector[i], S, vectorX1)
+    y3,x03,y_3,c3 = catenary(HorDif,-VerDif_vector[i], S, vectorX1)
 
-plt.plot(vectorX1,y2,'r')
-plt.plot(vectorX1+HorDif,y3+VerDif,'r') 
-plt.show()
+    #draw the catenary
+    plt.plot(vectorX1,y0,'b')
+    plt.plot(vectorX1+HorDif,y1,'b')
+
+    plt.plot(vectorX1,y2,'r')
+    plt.plot(vectorX1+HorDif,y3+VerDif_vector[i],'r') 
+    plt.pause(1)
+   
+ plt.show()
+
+
